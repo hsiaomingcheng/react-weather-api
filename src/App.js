ef3e378d8105f1710060cbf65511a96948561f00
Chris hsiao <chris.hsiao@igroup.tw>
2020-06-13 14:16:05 +0800

    [新增] 新增基底

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { hot } from 'react-hot-loader';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.props = props;
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <h1> Hello, World! </h1>
+            </div>
+        );
+    }
+}
+
+export default hot(module)(App);
